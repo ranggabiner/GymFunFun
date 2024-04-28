cd82c7540f92e6775263563d343d9e631cf8ac60
ranggaupnvj <2310501014@mahasiswa.upnvj.ac.id>
2024-04-29 00:08:33 +0700

    third commit

---
+++ GymFun/Views/MultiExerciseManager.swift
@@ -0,0 +1,18 @@
+//
+//  MultiExerciseManager.swift
+//  GymFun
+//
+//  Created by Rangga Biner on 29/04/24.
+//
+
+import SwiftUI
+
+struct MultiExerciseManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MultiExerciseManager()
+}
