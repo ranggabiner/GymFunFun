56b4d9dfb4c69aa46e5cc00ffe0b7a7acd91baf3
ranggaupnvj <2310501014@mahasiswa.upnvj.ac.id>
2024-04-29 08:42:05 +0700

    fifth commit

---
+++ GymFun/Views/StartView.swift
@@ -0,0 +1,18 @@
+//
+//  StartView.swift
+//  GymFun
+//
+//  Created by Rangga Biner on 29/04/24.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartView()
+}
